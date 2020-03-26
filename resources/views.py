8c1be99f80eaba3016da2a885f56981e245c92fb
baasare <asarebernard98@gmail.com>
2020-03-26 21:04:17 +0000

    empty queryset in resource display fixed

--- resources/views.py
+++ resources/views.py
@@ -6,5 +6,8 @@ from .models import Resource
 # @login_required
 def level_detail(request, level):
     resources = Resource.objects.filter(level=level)
-    current_level = resources[0].get_level_display
+    if resources:
+        current_level = resources[0].get_level_display
+    else:
+        current_level = "No Resource Added"
     return render(request, 'resources/level.html', {'resources': resources, 'current_level': current_level})
