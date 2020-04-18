aa14ae16acfb82e17b1715551caac563942ef027
baasare <asarebernard98@gmail.com>
2020-04-18 21:45:18 +0000

    indicator ordering

--- resources/views.py
+++ resources/views.py
@@ -6,7 +6,10 @@ from .models import Resource
 
 @login_required
 def level_detail(request, level):
-    resources = Resource.objects.filter(level=level)
+    resources = Resource.objects.filter(level=level).extra(
+        select={'myinteger': 'CAST(mycharfield AS INTEGER)'}
+    ).order_by('myinteger')
+    # resources = Resource.objects.filter(level=level)
     if resources:
         current_level = resources[0].get_level_display
     else:
