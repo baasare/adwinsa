88876183fcffc1daf1ccddf76523abf7b954faf7
baasare <asarebernard98@gmail.com>
2020-04-19 08:59:40 +0000

    pagination added

--- resources/views.py
+++ resources/views.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.decorators import login_required
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.shortcuts import render
 
 from .models import Resource
@@ -6,12 +7,21 @@ from .models import Resource
 
 @login_required
 def level_detail(request, level):
-    # resources = Resource.objects.filter(level=level).extra(
-    #     select={'myinteger': 'CAST(mycharfield AS INTEGER)'}
-    # ).order_by('myinteger')
-    resources = Resource.objects.filter(level=level)
+    resources_list = Resource.objects.filter(level=level)
+
+    page = request.GET.get('page', 1)
+
+    paginator = Paginator(resources_list, 10)
+    try:
+        resources = paginator.page(page)
+    except PageNotAnInteger:
+        resources = paginator.page(1)
+    except EmptyPage:
+        resources = paginator.page(paginator.num_pages)
+
     if resources:
         current_level = resources[0].get_level_display
     else:
         current_level = "No Resource Available"
-    return render(request, 'resources/level.html', {'resources': resources, 'current_level': current_level})
+    return render(request, 'resources/level.html',
+                  {'resources': resources, 'current_level': current_level, })
